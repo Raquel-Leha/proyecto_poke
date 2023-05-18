32d2099ba1a457b85451892ecc4bc1f27fa78a0f
Raquel_Leha <rlopezn833@gmail.com>
2023-05-18 11:17:12 +0200

    Codigo completamente revisado y debidamente comentado

--- pokedex.js
+++ pokedex.js
@@ -42,14 +42,18 @@ const mapPokeCharacters = (array) => {
 
 //Función manejadora del evento Click sobre la lista de los 150 pokemons
 const handlerClick1 = (ev) => {
- 
+
+//Variable que recoje un valor booleano true/false. 
   let a;
+//variable que recoje el id del element clikado
   let identity = ev.target.id;
-  console.log(identity);
+//variable que guarda el elemento del array mapeado que ha sido clikado.
+  let character = mappedPokeCharacters[identity - 1];
 
-  let heart = document.getElementsByClassName("heartDiv");
-  console.log(heart);
 
+/*Guardamos en el array heart los elementos con la clase heartDiv, y si el id de alguno de estos
+  elementos guardados en el array coincide con el id del elemento clicado, le cambiamos la clase*/
+  let heart = document.getElementsByClassName("heartDiv")
   for(const element of heart){
     if(element.id == identity){
       element.classList.remove("heartDiv");
@@ -58,36 +62,31 @@ const handlerClick1 = (ev) => {
     };
   };
   
-  let character = mappedPokeCharacters[identity - 1];
-
-
+  
+  
+  //Si la lista de favoritos esta vacia, guardamos el elemento clikado en favoritos y renderizamos
   if(arrayFavorites.length === 0){
-    
     arrayFavorites.push(character);
     renderFavorites(arrayFavorites);
   }else{
+
     for(let element of arrayFavorites){
-      
-  
       if(element.id == character.id){
-       
+        //Si hay coincidencia es porque ya se encuentra en la lista de favoritos 
         a = true;
         alert(element.name + " ya se encuentra en su lista de favoritos. No es posible añadirlo dos veces.");
         
       }
     };
-
+    //Si no hay coincidencia, pero la lista de favoritos(limitada a 5), ya se encuentra llena
     if(!a && arrayFavorites.length >=5){
-      alert("No es posible tener más de cinco pokemons favoritos.");
-     
-    }
-
+      alert("No es posible tener más de cinco pokemons favoritos.");  
+    };
+    //Si no existe coincidencia, y la lista de favoritos no esta llena
     if(!a && arrayFavorites.length<=4){
-
     arrayFavorites.push(character);
     renderFavorites(arrayFavorites);
-
-    }
+    };
  
   };
 
@@ -98,37 +97,37 @@ const handlerClick1 = (ev) => {
 //Función manejadora del event Click sobre la lista de los pokemons favoritos del usuario
 const handlerClick2 = (ev) => {
 
+//Guardamos en esta variable el id del elemento de favoritos que ha sido clikado
   let identity = ev.target.id;
   
 
+//Seleccionamos los elementos con la clase core(la clase de los favoritos) y los guardamos en el array heart
   let heart = document.getElementsByClassName("core");
-
   
+//Si algun elemento del array heart tiene el mismo id que el elemento clikado, entonces le cambiamos la clase
   for(const element of heart){
+
     if(element.id == identity){
-      console.log(element.id);
       element.classList.remove("core")
       element.classList.add("heartDiv");
     };
   };
 
-
+//Eliminamos del array de favoritos, el elemento clikado.
   for(let i= 0 ; i<arrayFavorites.length; i++){
    
     if(identity == arrayFavorites[i].id){
-      
       arrayFavorites.splice(i,1);
-      renderFavorites(arrayFavorites);
-      
+      renderFavorites(arrayFavorites);  
     };
   };
 
 
-
+//Recogemos en la variable misFavs el cambio que se ha producido en favoritos.
   localStorage.setItem("misFavs", JSON.stringify(arrayFavorites)); 
 };
 
-//Funcion que pinta en el HTML la lista de los pokemons favoritos de usuario
+//Funcion que pinta en el HTML la lista de los pokemons favoritos del usuario
 const renderFavorites = (array) => {
 
   pokeListFavorites$$.innerHTML = "";
@@ -180,7 +179,7 @@ const render = (array) => {
       div$$.setAttribute("id", character.id);
       div$$.classList.add("heartDiv");
 
-
+//Con este for verificamos si el pokemon se encuentra en favoritos, y si es así, le cambiamos la clase
       for(const element of arrayFavorites){
         if(character.id === element.id){
           div$$.classList.remove("heartDiv");
@@ -233,21 +232,24 @@ const searchPokemons = (pokemons, character) => {
 //Funcion que inicializa el programa
 const init = async () => {
 
-
+//Este array nos guarda todos los datos que nos devuelve la API sin filtrar
   const pokeCharacters = await getPokeCharacters();
-  
+//Este array guarda los datos de la API que nos interesan
   mappedPokeCharacters = mapPokeCharacters(pokeCharacters);
-
+//En el array favs se guardan los favoritos almacenados localmente 
   const favs = JSON.parse(localStorage.getItem("misFavs"));
+
+//y si favs no está vacio, llamamos a la funcion renderFavorites para que nos los pinte en la pagina
   if(favs){
     arrayFavorites = favs;
     renderFavorites(arrayFavorites);
   }
-
+//también llamamos a la función render para que nos pinte en pantalla los 150 pokemons
   render(mappedPokeCharacters);
-
+//Con esta función escuchamos el evento input y filtramos al pokemon seleccionado
   drawInput(mappedPokeCharacters);
 
 };
 
+//Inicializamos el programa
 init();
